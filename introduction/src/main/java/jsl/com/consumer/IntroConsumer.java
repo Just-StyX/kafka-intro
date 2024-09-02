545c79fee24400d65c1337d20302caa37e625277
Just-StyX <fjaccobian@gmail.com>
2024-09-02 11:54:07 -0400

    Introduction to kafka

---
+++ introduction/src/main/java/jsl/com/consumer/IntroConsumer.java
@@ -0,0 +1,37 @@
+package jsl.com.consumer;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Set;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+public class IntroConsumer {
+    public static void main(String[] args) {
+        final var topic = "introduction";
+
+        final Map<String, Object> configuration = Map.of(
+                BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
+                VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName(),
+                GROUP_ID_CONFIG, "introduction-group",
+                AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ENABLE_AUTO_COMMIT_CONFIG, false
+        );
+
+        try(var consumer = new KafkaConsumer<String, String>(configuration)) {
+            consumer.subscribe(Set.of(topic));
+
+            while (true) {
+                var records = consumer.poll(Duration.ofMillis(100));
+                for (var record: records) {
+                    System.out.printf("Got record with value %s%n", record.value());
+                }
+                consumer.commitAsync();
+            }
+        }
+    }
+}
---
+++ introduction/src/main/java/jsl/com/producer/IntroProducer.java
@@ -0,0 +1,42 @@
+package jsl.com.producer;
+
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class IntroProducer {
+    public static void main(String[] args) {
+        final var topic = "introduction";
+
+        final Map<String, Object> configuration = Map.of(
+                BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(),
+                VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(),
+                ENABLE_IDEMPOTENCE_CONFIG, true
+        );
+
+        try(var producer = new KafkaProducer<String, String>(configuration)) {
+            while (true) {
+                var key = "key";
+                var value = LocalDateTime.now().format(DateTimeFormatter.ofPattern("E MMM yyyy HH:mm:ss"));
+                System.out.printf("Publishing record with value %s%n", value);
+
+                Callback callback = (metadata, exception) -> {
+                    System.out.printf("Publishing with metadata: %s, error: %s%n", metadata, exception);
+                };
+                producer.send(new ProducerRecord<>(topic, key, value), callback);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
